7ea2e80a23fb68b19ee92f522ef1e5808283b04c
dcoplowe <david.coplowe@physics.ox.ac.uk>
2016-08-10 17:39:41 +0100

    Test build compiles

--- MomentumDists.cxx
+++ MomentumDists.cxx
@@ -26,10 +26,11 @@ using namespace std;
 using namespace PlotUtils;
 
 //These are the tuples used for this exercise
-const string filename_mc("/minerva/data/users/wospakrk/Minerva101Files/Tuples/CCInclusiveReco_MC_AnaTuple_minerva1.root");
+const string testing_mc("/pnfs/minerva/persistent/users/dcoplowe/CC1P1Pi_PL13C_080816/grid/central_value/minerva/ana/v10r8p9/00/01/32/00/SIM_minerva_00013200_Subruns_0001-0002-0003-0004_CC1P1PiAnalysis_Ana_Tuple_v10r8p9-dcoplowe.root");
+
+const string flag("CC1P1Pi");
 
 //This is the POT for Data and Monte Carlo for the tuples that we use in this exercise.
-const double total_data_pot = 2.0384e18;
 const double total_mc_pot = 1.79e19;
 
 void MomentumDists()
@@ -38,6 +39,47 @@ void MomentumDists()
     ROOT::Cintex::Cintex::Enable();
     
     cout<<"Producing Momentum Distributions for MC files."<<endl;
+    
+    TFile * infile = new TFile(testing_mc.c_str(), "READ");
+
+    TFile * outfile = new TFile("MomentumDists.root","RECREATE");
+    
+    TTree * intree;
+    infile->GetObject("CC1P1Pi",intree);
+    
+    //Produce independent particles vars hists:
+    
+    string part_snam[3] = {"mu","pr","pi"};
+    string part_name[3] = {"Muon", "Proton", "Pion"};
+    string part_symb[3] = {"#mu^{-}", "p", "#pi^{+}"};
+    
+    string var_unit[3] = {" (MeV/#it{c})", " (MeV/#it{c})", " (MeV)" };
+    
+    string rec_var[3] = {"mom", "pTMag", "E"};
+    string rec_name[3] = {"Momentum", "|#it{p}_{T}|", "Energy"};
+    
+    string true_var[3];// = {"turemom", "truepTMag", };
+    string true_name[3];// = {"Momentum", "|p_{T}|", "Energy"};
+    
+    for(int i = 0; i < n; i++){
+        true_var[i] = "true" + rec_var[i];
+        true_name[i] = "True " + rec_name[i];
+    }
+    
+    for(int i = 0; i < 3; i++){
+        cout << " Making Plots for " << part_name[i] << "." << endl;
+        
+        for(int j = 0; j < 3; j++){
+         
+            MnvH1D * rec_h = new MnvH1D( (part_snam[i] + "_" + rec_var[j]).c_str() , (part_name[i] + rec_name[j] + ";" + var_unit[j] + "; Counts").c_str(), 30, 0, 4000);
+            
+        }
+        
+
+        
+        
+    }
+    
     
 }
 
