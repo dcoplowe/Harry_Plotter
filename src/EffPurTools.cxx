a0b319969a3b71fa9bab2e150321e698a0c85821
dcoplowe <david.coplowe@physics.ox.ac.uk>
2016-08-15 21:18:24 +0100

    First eff. plotting code

--- src/EffPurTools.cxx
+++ src/EffPurTools.cxx
@@ -1,1 +1,22 @@
 #include "EffPurTools.h"
+
+using namespace std;
+
+EffPurTools::EffPurTools(){
+    cout << "EffPurTools::EffPurTools" << endl;
+}
+
+//These may not be void functions:
+void EffPurTools::EffVSCuts(){
+    cout << "EffPurTools::EffVSCuts" << endl;
+}
+
+void EffPurTools::EffVSVar(){
+    cout << "EffPurTools::EffVSVar" << endl;
+}
+
+void EffPurTools::PurVSCuts(){
+    cout << "EffPurTools::PurVSCuts" << endl;
+}
+
+
--- src/EffPurTools.h
+++ src/EffPurTools.h
@@ -1,4 +1,19 @@
 #ifndef EFFPURTOOLS_H
 #define EFFPURTOOLS_H
+
+class EffPurTools {
+public:
+    
+    EffPurTools();
+    ~EffPurTools(){};
+    
+    //These may not be void functions:
+    void EffVSCuts();
+    void EffVSVar();
+    
+    void PurVSCuts();
+    
+};
+
 #endif
 
