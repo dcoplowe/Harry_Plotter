8f8ba75ec1244a1ef660635f7adfaf30154e0c88
dcoplowe <david.coplowe@physics.ox.ac.uk>
2016-08-16 17:24:55 +0100

    Makefile work for plotting code

--- EffPurTools.cxx
+++ EffPurTools.cxx
@@ -4,23 +4,52 @@
 #include <cstdio>
 #include <cstdlib>
 
+#include "TString.h"
+
 using namespace std;
 
+EffPurTools::EffPurTools(TString filename, TString truename, TString reconame){
+    
+    cout << "EffPurTools::EffPurTools(TString, TString, TString)" << endl;
+    SetFile(filename);
+    SetRecoBranch(reconame);
+    SetTrueBranch(truename);
+    
+    cout << "    Filename: " << _filename.Data() << endl;
+    cout << "Truth branch: " << _truename.Data() << endl;
+    cout << "Recon branch: " << _reconame.Data() << endl;
+    
+}
+
 EffPurTools::EffPurTools(){
-    cout << "EffPurTools::EffPurTools" << endl;
+    cout << "EffPurTools::EffPurTools()" << endl;
+    this->EffPurTools("UNKNOWN");
+    //SetRecoBranch("CC1P1Pi");
+    //SetTrueBranch("Truth");
 }
 
 //These may not be void functions:
-void EffPurTools::EffVSCuts(){
-    cout << "YEAHHH::EffPurTools::EffVSCuts" << endl;
+void EffPurTools::EffVSCuts(TString signal, TString cuts){
+    cout << "EffPurTools::EffVSCuts(TString, TString)" << endl;
+    
+    cout << "    Signal: " << signal.Data() << endl;
+    cout << "    Cut(s): " << cuts.Data() << endl;
+
+
 }
 
-void EffPurTools::EffVSVar(){
-    cout << "EffPurTools::EffVSVar" << endl;
+void EffPurTools::EffVSVar(TString var, TString signal, TString cuts){
+    cout << "EffPurTools::EffVSVar(TString, TString, TString)" << endl;
+    cout << "    Signal: " << signal.Data() << endl;
+    cout << "    Cut(s): " << cuts.Data() << endl;
+
 }
 
-void EffPurTools::PurVSCuts(){
-    cout << "EffPurTools::PurVSCuts" << endl;
+void EffPurTools::PurVSCuts(TString signal, TString cuts){
+    cout << "EffPurTools::PurVSCuts(TString, TString)" << endl;
+    cout << "    Signal: " << signal.Data() << endl;
+    cout << "    Cut(s): " << cuts.Data() << endl;
+
 }
 
 
--- EffPurTools.h
+++ EffPurTools.h
@@ -1,19 +1,49 @@
 #ifndef EFFPURTOOLS_H
 #define EFFPURTOOLS_H
 
+//#include <PlotUtils/MnvH1D.h>
+
+//Forward declarations here:
+class TString;
+
 using namespace std;
 
 class EffPurTools {
 public:
     
+    //Given that all eff. is based on truth info. and pur.
+    //on the reco. these branch names will always remain
+    //the same. We can therefore load the file into the tool
+    //and run everything within the class.
+    
+    //For now have assume a file is read in.
+    
+    EffPurTools(TString filename, TString truename = "Truth", TString TString reconame = "CC1P1Pi");
     EffPurTools();
+    
     ~EffPurTools(){};
     
-    //These may not be void functions:
-    void EffVSCuts();
-    void EffVSVar();
+    void SetFile(TString var){ _filename = var };
+    void SetRecoBranch(TString var){ _reconame = var };
+    void SetTrueBranch(TString var){ _truename = var };
     
-    void PurVSCuts();
+    void SetGlobalSignal(TString var){ _glob_signal = var };
+    
+    //These may not be void functions:
+    void EffVSCuts(TString signal, TString cuts = "");
+    void EffVSVar(TString var, TString signal, TString cuts = "");
+    
+    void PurVSCuts(TString signal, TString cuts = "");
+    
+private:
+    //File and directory info:
+    TString _filename;
+    TString _truename;
+    TString _reconame;
+    
+    //Global signal information
+    TString _glob_signal;
+    
     
 };
 
