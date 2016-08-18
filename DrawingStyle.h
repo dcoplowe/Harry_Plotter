99cde3b10474737a89dd51a60ec5d7f42dcb13f7
dcoplowe <david.coplowe@physics.ox.ac.uk>
2016-08-18 19:04:40 +0100

    DrawingStyle header added. Test to see if it works.

--- DrawingStyle.h
+++ DrawingStyle.h
@@ -1,14 +1,14 @@
 #ifndef DRAWING_STYLE_H
 #define DRAWING_STYLE_H
 
-#include <TROOT.h>
-#include <TStyle.h>
+//#include <TROOT.h>
+//#include <TStyle.h>
 
 class DrawingStyle {
 
 public:
     DrawingStyle(){
-        gStyle->SetLegendBorderSize(0);
+        //gStyle->SetLegendBorderSize(0);
     }
     
     ~DrawingStyle(){;}
