eb7a257db5910d38da0c5626a941a0eb5839959a
dcoplowe <david.coplowe@physics.ox.ac.uk>
2016-08-22 17:42:17 +0100

    Momentum Breakdown plots

--- MomentumDists.cxx
+++ MomentumDists.cxx
@@ -142,7 +142,7 @@ void MomentumDists()
         leg_part->AddEntry(s_h_mom_p3, Form("#mu^{-} (%.2f%%)",s_h_mom_p3_i), "f");
         leg_part->AddEntry(s_h_mom_p4, Form("Other (%.2f%%)",s_h_mom_p4_i), "f");
                      
-        TCanvas * c_parts = new TCanvas(Form("%s_mom_part_breakdown",tmp_part_name.Data()), "", 600, 600);
+        TCanvas * c_parts = new TCanvas(Form("%s_mom_part_breakdown",tmp_part_name.Data()), "", 700, 700);
         c_parts->cd();
         hs_part->Draw();
         TLegend * pot_parts = plot->GetPOT(0.7,0.8);
@@ -159,10 +159,10 @@ void MomentumDists()
         MnvH1D * h_mom_i5 = plot->GetRecoHisto(Form("CC1P1Pi_%s_mom",tmp_part_snam.Data()),20, 0., 4000., Form("#it{p}_{%s} (MeV/#it{c})",tmp_part_symb.Data()), Form("%s && mc_current != 1", common_cuts_p.Data()));//NC/no current
         h_mom_i4->Add(h_mom_i5);//Add NC ints to other.
         
-        TH1D * s_h_mom_i1 = new TH1D(h_mom_i1->GetStatError(true));
-        TH1D * s_h_mom_i2 = new TH1D(h_mom_i2->GetStatError(true));
-        TH1D * s_h_mom_i3 = new TH1D(h_mom_i3->GetStatError(true));
-        TH1D * s_h_mom_i4 = new TH1D(h_mom_i4->GetStatError(true));
+        TH1D * s_h_mom_i1 = new TH1D(h_mom_i1->GetStatError());
+        TH1D * s_h_mom_i2 = new TH1D(h_mom_i2->GetStatError());
+        TH1D * s_h_mom_i3 = new TH1D(h_mom_i3->GetStatError());
+        TH1D * s_h_mom_i4 = new TH1D(h_mom_i4->GetStatError());
         
         plot->ColFill(s_h_mom_i1, DrawingStyle::DSQes);
         plot->ColFill(s_h_mom_i2, DrawingStyle::DSRes);
@@ -193,7 +193,7 @@ void MomentumDists()
         leg_ints->AddEntry(s_h_mom_i3, Form("CC DIS (%.2f%%)",s_h_mom_i3_i), "f");
         leg_ints->AddEntry(s_h_mom_i4, Form("Other (inc. NC) (%.2f %%)",s_h_mom_i4_i), "f");
         
-        TCanvas * c_ints = new TCanvas(Form("%s_mom_ints_breakdown",tmp_part_name.Data()), "", 500, 500);
+        TCanvas * c_ints = new TCanvas(Form("%s_mom_ints_breakdown",tmp_part_name.Data()), "", 700, 700);
         c_ints->cd();
         hs_ints->Draw();
         TLegend * pot_ints = plot->GetPOT(0.7,0.8);
