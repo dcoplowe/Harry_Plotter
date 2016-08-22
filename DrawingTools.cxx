621b7174de13bafeb454329faf407afddc92abc8
dcoplowe <david.coplowe@physics.ox.ac.uk>
2016-08-22 12:42:15 +0100

    Momentum Breakdown plots

--- DrawingTools.cxx
+++ DrawingTools.cxx
@@ -172,7 +172,7 @@ MnvH1D * DrawingTools::GetTruthHisto(const TString var, int nbins, const Double_
 }
 
 MnvH1D * DrawingTools::GetRecoHisto(const TString var, int nbins, const double x_low, const double x_high, const TString xy_title, const TString cuts){
-    cout << "DrawingTools::GetRecoHisto" << endl;
+    cout << "DrawingTools::GetRecoHisto(const TString var, int nbins, const double x_low, const double x_high, const TString xy_title, const TString cuts)" << endl;
     
     if(_file->IsZombie()){
         cout << "File is Zombie" << endl;
