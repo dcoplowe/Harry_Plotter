4c37a68b8d24e7424f69299a0561d7e43aae9256
dcoplowe <david.coplowe@physics.ox.ac.uk>
2016-10-05 16:40:01 +0100

    Plots for MIN Col. Mtg

--- DrawingTools.cxx
+++ DrawingTools.cxx
@@ -311,10 +311,8 @@ KinMap DrawingTools::KinArray(TTree * intree, const TString vars_tr, int re_nbin
         map.truth->GetYaxis()->SetTitle("Counts");
         
         if(!tmp_cuts.EqualTo("", TString::kExact)) tmp_cuts.Append(" && ");
-        tmp_cuts.Append(Form("%f < %s && %s < %f && %f < %s && %s < %f ", re_bins[0], recon_var.Data(), recon_var.Data(), re_bins[re_nbins + 1], tr_bins[0],
-                             truth_var.Data(), truth_var.Data(), tr_bins[tr_nbins + 1]));
-        
-        cout << "re_bins[0] = " << re_bins[0] << " re_bins[re_nbins + 1] = " << re_bins[re_nbins] << " tr_bins[0] = " << tr_bins[0] << " tr_bins[tr_nbins + 1] = " << tr_bins[tr_nbins + 1] << endl;
+        tmp_cuts.Append(Form("%f < %s && %s < %f && %f < %s && %s < %f ", re_bins[0], recon_var.Data(), recon_var.Data(), re_bins[re_nbins], tr_bins[0],
+                             truth_var.Data(), truth_var.Data(), tr_bins[tr_nbins]));
         
         cout << tmp_cuts << endl;
     }
