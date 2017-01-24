0ffe9a9a714420365f4d43941ce45f1c3b55c50f
dcoplowe <david.coplowe@physics.ox.ac.uk>
2017-01-24 17:09:54 +0000

    T2K Minerva plotting

--- MomentumDists.cxx
+++ MomentumDists.cxx
@@ -662,7 +662,7 @@ void MomentumDists::MakePlots(){
     theta.savename = m_proton->theta;
     theta.symbol = "#theta_{p}";
     
-    BDCans pr_theta_top = run->TOPO(theta, 40, 0, TMath::Pi(), base_cut);
+    BDCans pr_theta_top = run->TOPO(theta, 40, 0, 3.14, base_cut);
     
     outfile->cd();
     
@@ -686,7 +686,7 @@ void MomentumDists::MakePlots(){
     pr_theta_top_logo_smearSN->Draw();
     pr_theta_top.smearSN->Write();
     
-    BDCans pr_theta_tar = run->TARGET(theta, 40, 0, TMath::Pi(), base_cut);
+    BDCans pr_theta_tar = run->TARGET(theta, 40, 0, 3.14, base_cut);
     
     pr_theta_tar.recon->cd();
     TLatex * pr_theta_tar_logo_recon = GetLogo();
@@ -698,7 +698,7 @@ void MomentumDists::MakePlots(){
     pr_theta_tar_logo_truth->Draw();
     pr_theta_tar.truth->Write();
     
-    BDCans pr_theta_pid = run->PID(theta, 40, 0, TMath::Pi(), m_proton->pdg, base_cut);
+    BDCans pr_theta_pid = run->PID(theta, 40, 0, 3.14, m_proton->pdg, base_cut);
     
     pr_theta_pid.recon->cd();
     TLatex * pr_theta_pid_logo_recon = GetLogo();
@@ -716,7 +716,7 @@ void MomentumDists::MakePlots(){
     theta.savename = m_pion->theta;
     theta.symbol = "#theta_{#pi^{#pm}}";
     
-    BDCans pi_theta_top = run->TOPO(theta, 40, 0, TMath::Pi(), base_cut);
+    BDCans pi_theta_top = run->TOPO(theta, 40, 0, 3.14, base_cut);
     
     outfile->cd();
     
@@ -740,7 +740,7 @@ void MomentumDists::MakePlots(){
     pi_theta_top_logo_smearSN->Draw();
     pi_theta_top.smearSN->Write();
     
-    BDCans pi_theta_tar = run->TARGET(theta, 40, 0, TMath::Pi(), base_cut);
+    BDCans pi_theta_tar = run->TARGET(theta, 40, 0, 3.14, base_cut);
     
     pi_theta_tar.recon->cd();
     TLatex * pi_theta_tar_logo_recon = GetLogo();
@@ -752,7 +752,7 @@ void MomentumDists::MakePlots(){
     pi_theta_tar_logo_truth->Draw();
     pi_theta_tar.truth->Write();
     
-    BDCans pi_theta_pid = run->PID(theta, 40, 0, TMath::Pi(), m_pion->pdg, base_cut);
+    BDCans pi_theta_pid = run->PID(theta, 40, 0, 3.14, m_pion->pdg, base_cut);
     
     pi_theta_pid.recon->cd();
     TLatex * pi_theta_pid_logo_recon = GetLogo();
@@ -770,7 +770,7 @@ void MomentumDists::MakePlots(){
     theta.savename = m_muon->theta;
     theta.symbol = "#theta_{#mu^{#pm}}";
     
-    BDCans mu_theta_top = run->TOPO(theta, 40, 0, TMath::Pi(), base_cut);
+    BDCans mu_theta_top = run->TOPO(theta, 40, 0, 3.14, base_cut);
     
     outfile->cd();
     
@@ -794,7 +794,7 @@ void MomentumDists::MakePlots(){
     mu_theta_top_logo_smearSN->Draw();
     mu_theta_top.smearSN->Write();
     
-    BDCans mu_theta_tar = run->TARGET(theta, 40, 0, TMath::Pi(), base_cut);
+    BDCans mu_theta_tar = run->TARGET(theta, 40, 0, 3.14, base_cut);
     
     mu_theta_tar.recon->cd();
     TLatex * mu_theta_tar_logo_recon = GetLogo();
@@ -806,7 +806,7 @@ void MomentumDists::MakePlots(){
     mu_theta_tar_logo_truth->Draw();
     mu_theta_tar.truth->Write();
     
-    BDCans mu_theta_pid = run->PID(theta, 40, 0, TMath::Pi(), m_muon->pdg, base_cut);
+    BDCans mu_theta_pid = run->PID(theta, 40, 0, 3.14, m_muon->pdg, base_cut);
     
     mu_theta_pid.recon->cd();
     TLatex * mu_theta_pid_logo_recon = GetLogo();
@@ -829,7 +829,7 @@ void MomentumDists::MakePlots(){
     phi.savename = m_proton->phi;
     phi.symbol = "#phi_{p}";
     
-    BDCans pr_phi_top = run->TOPO(phi, 40, 0, TMath::Pi(), base_cut);
+    BDCans pr_phi_top = run->TOPO(phi, 40, 0, 3.14, base_cut);
     
     outfile->cd();
     
@@ -853,7 +853,7 @@ void MomentumDists::MakePlots(){
     pr_phi_top_logo_smearSN->Draw();
     pr_phi_top.smearSN->Write();
     
-    BDCans pr_phi_tar = run->TARGET(phi, 40, 0, TMath::Pi(), base_cut);
+    BDCans pr_phi_tar = run->TARGET(phi, 40, 0, 3.14, base_cut);
     
     pr_phi_tar.recon->cd();
     TLatex * pr_phi_tar_logo_recon = GetLogo();
@@ -865,7 +865,7 @@ void MomentumDists::MakePlots(){
     pr_phi_tar_logo_truth->Draw();
     pr_phi_tar.truth->Write();
     
-    BDCans pr_phi_pid = run->PID(phi, 40, 0, TMath::Pi(), m_proton->pdg, base_cut);
+    BDCans pr_phi_pid = run->PID(phi, 40, 0, 3.14, m_proton->pdg, base_cut);
     
     pr_phi_pid.recon->cd();
     TLatex * pr_phi_pid_logo_recon = GetLogo();
@@ -884,7 +884,7 @@ void MomentumDists::MakePlots(){
     phi.savename = m_pion->phi;
     phi.symbol = "#phi_{#pi^{#pm}}";
     
-    BDCans pi_phi_top = run->TOPO(phi, 40, 0, TMath::Pi(), base_cut);
+    BDCans pi_phi_top = run->TOPO(phi, 40, 0, 3.14, base_cut);
     
     outfile->cd();
     
@@ -908,7 +908,7 @@ void MomentumDists::MakePlots(){
     pi_phi_top_logo_smearSN->Draw();
     pi_phi_top.smearSN->Write();
     
-    BDCans pi_phi_tar = run->TARGET(phi, 40, 0, TMath::Pi(), base_cut);
+    BDCans pi_phi_tar = run->TARGET(phi, 40, 0, 3.14, base_cut);
     
     pi_phi_tar.recon->cd();
     TLatex * pi_phi_tar_logo_recon = GetLogo();
@@ -920,7 +920,7 @@ void MomentumDists::MakePlots(){
     pi_phi_tar_logo_truth->Draw();
     pi_phi_tar.truth->Write();
     
-    BDCans pi_phi_pid = run->PID(phi, 40, 0, TMath::Pi(), m_pion->pdg, base_cut);
+    BDCans pi_phi_pid = run->PID(phi, 40, 0, 3.14, m_pion->pdg, base_cut);
     
     pi_phi_pid.recon->cd();
     TLatex * pi_phi_pid_logo_recon = GetLogo();
@@ -938,7 +938,7 @@ void MomentumDists::MakePlots(){
     phi.savename = m_muon->phi;
     phi.symbol = "#phi_{#mu^{#pm}}";
     
-    BDCans mu_phi_top = run->TOPO(phi, 40, 0, TMath::Pi(), base_cut);
+    BDCans mu_phi_top = run->TOPO(phi, 40, 0, 3.14, base_cut);
     
     outfile->cd();
     
@@ -962,7 +962,7 @@ void MomentumDists::MakePlots(){
     mu_phi_top_logo_smearSN->Draw();
     mu_phi_top.smearSN->Write();
     
-    BDCans mu_phi_tar = run->TARGET(phi, 40, 0, TMath::Pi(), base_cut);
+    BDCans mu_phi_tar = run->TARGET(phi, 40, 0, 3.14, base_cut);
     
     mu_phi_tar.recon->cd();
     TLatex * mu_phi_tar_logo_recon = GetLogo();
@@ -974,7 +974,7 @@ void MomentumDists::MakePlots(){
     mu_phi_tar_logo_truth->Draw();
     mu_phi_tar.truth->Write();
     
-    BDCans mu_phi_pid = run->PID(phi, 40, 0, TMath::Pi(), m_muon->pdg, base_cut);
+    BDCans mu_phi_pid = run->PID(phi, 40, 0, 3.14, m_muon->pdg, base_cut);
     
     mu_phi_pid.recon->cd();
     TLatex * mu_phi_pid_logo_recon = GetLogo();
