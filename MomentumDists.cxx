75486c8b01349061f2bc04f5d942c681866f49c6
dcoplowe <david.coplowe@physics.ox.ac.uk>
2017-01-24 01:17:51 +0000

    T2K Minerva plotting

--- MomentumDists.cxx
+++ MomentumDists.cxx
@@ -457,7 +457,7 @@ void MomentumDists::MakePlots(){
     TH2D * hist_1 = test->GetHisto( (m_proton->trueP + ":" + m_proton->P).c_str() , 30, 0., 2000.0, 30, 0., 2000.0, "proton p (MeV/c)","accum_level>5 ");
     TH1D * hist_2 = test->GetHisto(m_recovars->dpTT, 59, -300., 300, "#deltap_{TT} (MeV/c)","accum_level>5 ");
     
-    DrawingTools::KinMap pmap = test->KinArray( (m_proton->pTT + ":" + m_proton->pTT).c_str(), 29, -1000, 1000, "proton p_{TT} (MeV/c)", "accum_level>5 ");
+    DrawingTools::KinMap pmap = test->KinArray( (m_proton->trueP + ":" + m_proton->P).c_str(), 29, -1000, 1000, "proton p_{TT} (MeV/c)", "accum_level>5 ");
     
     outfile->cd();
     hist_1->Write();
