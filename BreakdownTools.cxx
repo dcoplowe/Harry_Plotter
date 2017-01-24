3bb97457e59bdfa93a7a1481581b80c0ebe58417
dcoplowe <david.coplowe@physics.ox.ac.uk>
2017-01-24 04:00:17 +0000

    T2K Minerva plotting

--- BreakdownTools.cxx
+++ BreakdownTools.cxx
@@ -11,8 +11,8 @@
 
 BreakdownTools::BreakdownTools(std::string filename, std::string treename) : DrawingTools(filename, treename, ("BD" + treename).c_str()), m_printPOT(false), m_fullbreakdown(false){
 
-    PDGs proton(2212, "proton",   "p");
-    m_pdglist.push_back(proton);
+//    PDGs proton(2212, "proton",   "p");
+//    m_pdglist.push_back(proton);
 //    m_pdglist.push_back( PDGs(211,  "piplus",   "#pi^{+}") );
 //    m_pdglist.push_back( PDGs(-211, "piminus", "#pi^{-}") );
 //    m_pdglist.push_back( PDGs(13, "muon", "#muon^{-}") );
@@ -29,52 +29,52 @@ BreakdownTools::~BreakdownTools(){
 
 BDCans BreakdownTools::PID(Variable var, Int_t nbins, Double_t * bins, std::string pdgvar, std::string cuts){
 
-    std::vector<DrawingTools::KinMap> kinmap_list;
-    
-    std::string other_cut = cuts;
-    
-    std::string tmp_cuts_1 = cuts;
-    if(!cuts.empty()){
-        tmp_cuts_1 += " && ";
-        other_cut += " && (";
-    }
-    
-    std::vector<std::string> particle_symbs;
-    
-    for(int i = 0; i < (int)m_pdglist.size(); i++){
-    
-        PDGs particle = m_pdglist[i];
-        particle_symbs.push_back(particle.symbol);
-        
-        std::string tmp_cuts = tmp_cuts_1;
-        tmp_cuts += pdgvar;
-        tmp_cuts += " == ";
-        tmp_cuts += particle.pdg_s;
-        
-        cout << "tmp_cuts: " << tmp_cuts << endl;
-        
-        if(other_cut.empty()){
-            other_cut = pdgvar;
-            other_cut += " != ";
-            other_cut += particle.pdg_s;
-        }
-        else{
-            other_cut += " || ";
-            other_cut += pdgvar;
-            other_cut += " != ";
-            other_cut += particle.pdg_s;
-        }
-            
-        DrawingTools::KinMap tmp_kinmap = KinArray(var.name, nbins, bins, (var.symbol + " (" + var.units + ")" ).c_str(), tmp_cuts);
-        kinmap_list.push_back(tmp_kinmap);
-    }
-    
-    if(!cuts.empty()) other_cut += ")";
-    
-    cout << "other_cut: " << other_cut << endl;
-    
-    DrawingTools::KinMap other_kinmap = KinArray(var.name, nbins, bins, (var.symbol + " (" + var.units + ")" ).c_str(), other_cut);
-    kinmap_list.push_back(other_kinmap);
+//    std::vector<DrawingTools::KinMap> kinmap_list;
+//    
+//    std::string other_cut = cuts;
+//    
+//    std::string tmp_cuts_1 = cuts;
+//    if(!cuts.empty()){
+//        tmp_cuts_1 += " && ";
+//        other_cut += " && (";
+//    }
+//    
+//    std::vector<std::string> particle_symbs;
+//    
+//    for(int i = 0; i < (int)m_pdglist.size(); i++){
+//    
+//        PDGs particle = m_pdglist[i];
+//        particle_symbs.push_back(particle.symbol);
+//        
+//        std::string tmp_cuts = tmp_cuts_1;
+//        tmp_cuts += pdgvar;
+//        tmp_cuts += " == ";
+//        tmp_cuts += particle.pdg_s;
+//        
+//        cout << "tmp_cuts: " << tmp_cuts << endl;
+//        
+//        if(other_cut.empty()){
+//            other_cut = pdgvar;
+//            other_cut += " != ";
+//            other_cut += particle.pdg_s;
+//        }
+//        else{
+//            other_cut += " || ";
+//            other_cut += pdgvar;
+//            other_cut += " != ";
+//            other_cut += particle.pdg_s;
+//        }
+//            
+//        DrawingTools::KinMap tmp_kinmap = KinArray(var.name, nbins, bins, (var.symbol + " (" + var.units + ")" ).c_str(), tmp_cuts);
+//        kinmap_list.push_back(tmp_kinmap);
+//    }
+//    
+//    if(!cuts.empty()) other_cut += ")";
+//    
+//    cout << "other_cut: " << other_cut << endl;
+//    
+//    DrawingTools::KinMap other_kinmap = KinArray(var.name, nbins, bins, (var.symbol + " (" + var.units + ")" ).c_str(), other_cut);
+//    kinmap_list.push_back(other_kinmap);
     
     BDCans cans;
     
