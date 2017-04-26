c459c8078973056bbe74e605bd9ccf4d649f99d4
dcoplowe <david.coplowe@physics.ox.ac.uk>
2017-04-26 10:33:04 +0100

    Readying code for use on both T2K and MINERvA

--- DataClasses.cxx
+++ DataClasses.cxx
@@ -298,7 +298,7 @@ Particle::Particle(Experiment::Name exp, std::string name, std::string tag) : m_
 
     if(exp == Experiment::T2K){
         //Reco vars: Common:
-        P(m_tag + name + "_mom",  0., 2000.);
+        P(m_tag + name + "_mom", 20, 0., 2000.);
         // pT =        m_tag + name + "_pT";
         // pTMag =     m_tag + name + "_pTMag";
         // startdir =  m_tag + name + "_dir";
@@ -355,7 +355,7 @@ Particle::Particle(Experiment::Name exp, std::string name, std::string tag) : m_
         }
         
         //Reco vars: Common:
-        P(m_tag + name + "_mom",  0., 2000.);
+        P(m_tag + name + "_mom", 40,  0., 2000.);
         // pT =        m_tag + name + "_pT";
         // pTMag =     m_tag + name + "_pTMag";
         // startdir =  m_tag + tmpname + "_startdir";
