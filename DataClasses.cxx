3fd380accde80a93e7e4a32953a3680a90feb403
dcoplowe <david.coplowe@physics.ox.ac.uk>
2017-04-27 18:05:38 +0100

    Readying code for use on both T2K and MINERvA

--- DataClasses.cxx
+++ DataClasses.cxx
@@ -13,7 +13,7 @@ using std::endl;
 namespace print_level {
     bool debug = true;// = true;// = true;
     bool quiet = false;
-    bool effpurtools = true;
+    bool effpurtools = false;
     bool drawingtools = false;
 }
 
