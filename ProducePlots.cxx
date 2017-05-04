5d9c1d8ebaf2d974c9df4715453ba8219f999618
dcoplowe <david.coplowe@physics.ox.ac.uk>
2017-05-04 11:23:10 +0100

    Makefile ammendments

--- ProducePlots.cxx
+++ ProducePlots.cxx
@@ -295,7 +295,7 @@ void ProducePlots::PositionPlot(Variable var, Int_t nbins, Double_t * bins, std:
          cout << "x_range = " << x_range << " x_scale = " << x_scale << endl;
 
          double x_width = (t2kgeometry::fgd1max[xyz] -  t2kgeometry::fgd1min[xyz])/2;
-         double x_center = t2kgeometry::fgd1min[xyz] + x_width + 2*x_scale;
+         double x_center = t2kgeometry::fgd1min[xyz] + x_width - x_scale;
 
          x_width *= 1/x_range;
          x_center *= 1/x_range;
