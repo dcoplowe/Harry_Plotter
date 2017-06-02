11b241114762ab29d41d37e3d722dc9a3b2b8385
dcoplowe <david.coplowe@physics.ox.ac.uk>
2017-06-02 13:55:39 +0100

    Base for breakdown tools code

--- DrawingTools.cxx
+++ DrawingTools.cxx
@@ -512,6 +512,9 @@ TH1D * DrawingTools::GetHistFromStack(THStack * stack)
     TIter next(slist);
     m_1Dcounter++;
     TH1D * sum = new TH1D(Form("%s_sum%.3d", hfirst->GetName(), m_1Dcounter), "", hfirst->GetXaxis()->GetNbins(), hfirst->GetXaxis()->GetXbins()->GetArray());
+    cout << "hfirst hfirst hfirst: Low " << sum->GetXaxis()->GetXmin() << " High " << sum->GetXaxis()->GetXmax() << endl;
+
+
     TH1D * shist_tmp;
     while ( (shist_tmp = (TH1D*)next()) ) {
         cout << "DrawingTools::GetHistFromStack : Adding Hist" << endl;
