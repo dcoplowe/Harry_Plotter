91f87106433bb3dddf09b65ae1dba0e7e9c1c21a
dcoplowe <david.coplowe@physics.ox.ac.uk>
2017-08-09 14:56:08 +0100

    Working on simple plotting code

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 swdir=/data/t2k/coplowe/software
 export ROOTSYS=${swdir}/rootpyth/root/
-export HP_ROOT=${swdir}/CompareMC
+export HP_ROOT=${swdir}/Harry_Plotter
 export PATH=$PATH:${HP_ROOT}
 source ${ROOTSYS}/bin/thisroot.sh
 
