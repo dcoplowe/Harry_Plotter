90edc95f98d64f2a9b598298369e22559201ab8a
dcoplowe <david.coplowe@physics.ox.ac.uk>
2017-08-10 15:56:42 +0100

    Working on simple plotting code

--- src/base/DrawingTools.cxx
+++ src/base/DrawingTools.cxx
@@ -38,7 +38,7 @@ DrawingTools::DrawingTools(std::string filename, std::string treename, std::stri
 
     assert(m_tree);
     
-    if(MaxEntries == 80085) MaxEntries = kMaxEntries;
+    if(MaxEntries == 80085) MaxEntries = TVirtualTreePlayer::kMaxEntries;
 
     // s_1Dcounter = -1;
     // s_2Dcounter = -1;
