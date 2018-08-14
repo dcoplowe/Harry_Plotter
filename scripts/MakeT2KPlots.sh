b93f4673a517f6166341184f4b0344eb3d12828b
dcoplowe <david.coplowe@physics.ox.ac.uk>
2018-08-14 14:03:19 +0100

    Refining backgrounds

--- scripts/MakeT2KPlots.sh
+++ scripts/MakeT2KPlots.sh
@@ -5,9 +5,6 @@ plotter=/data/t2k/coplowe/software/Harry_Plotter/
 
 PATH=${PATH}:${plotter}
 
-fgd1=/data/t2k/coplowe/numuCC1P1PiOutput/march17/neutP6BWA/FGD1/nall_data_FGD1_type1_wome_fboff_minaclev0_310517/allTree.root
-fgd2=/data/t2k/coplowe/numuCC1P1PiOutput/march17/neutP6BWA/FGD2/nall_data_FGD2_type1_wome_fboff_minaclev0_090818/allTree.root
-
 infile=$1
 opt=$2
 cutson=$3
@@ -31,5 +28,6 @@ cd ${dir}
 pwd
 
 echo "Starting to produce plots."
-ProducePlots -t -i ${infile} -o ${dir}/${savename} ${opt} ${cutson} -b 1
+ProducePlots -t -i ${infile} -o ${dir}/${savename} ${opt} ${cutson}
+# -b 1
 echo "Finished making plots."
---
+++ scripts/ProduceThesisPlots.sh
@@ -0,0 +1,23 @@
+fgd1=/data/t2k/coplowe/numuCC1P1PiOutput/march17/neutP6BWA/FGD1/nall_data_FGD1_type1_wome_fboff_minaclev0_310517/allTree.root
+# fgd1=/data/t2k/coplowe/numuCC1P1PiOutput/march17/neutP6BWA/FGD1/nall_data_FGD1_type1_wome_fboff_minaclev0_100817/allTree.root
+fgd2=/data/t2k/coplowe/numuCC1P1PiOutput/march17/neutP6BWA/FGD2/nall_data_FGD2_type1_wome_fboff_minaclev0_090818/allTree.root
+
+jno=0
+for opt in 0 1 4 6 13 28; do
+	for fgd in $fgd1 $fgd2; do
+		jno=$(expr $jno + 1)
+		jobfile=job${jno}.sh
+		cat > $jobfile <<EOF
+MakeT2KPlots.sh $fgd $opt
+EOF
+		chmod +x $jobfile
+		qsub -l -cput=02:00:00 -N $jobfile ./$jobfile
+		qsubresult=$?
+		while ! [ $qsubresult  -eq 0 ];do
+			sleep 4
+			qsub -l cput=02:00:00 -N $jobfile  ./$jobfile
+			qsubresult=$?
+		done
+	done 
+done
+echo "Done"
