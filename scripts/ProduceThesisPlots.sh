f87f822677c626cd099d5f1384a07907e998ad68
dcoplowe <david.coplowe@physics.ox.ac.uk>
2018-08-14 15:19:44 +0100

    Refining backgrounds

--- scripts/ProduceThesisPlots.sh
+++ scripts/ProduceThesisPlots.sh
@@ -1,5 +1,5 @@
 # fgd1=/data/t2k/coplowe/numuCC1P1PiOutput/march17/neutP6BWA/FGD1/nall_data_FGD1_type1_wome_fboff_minaclev0_310517/allTree.root
-fgd1=/data/t2k/coplowe/numuCC1P1PiOutput/march17/neutP6BWA/FGD1/nall_data_FGD1_type1_wome_fboff_minaclev0_100817/allTree.root
+fgd1=/data/t2k/coplowe/numuCC1P1PiOutput/march17/neutP6BWA/FGD1/nall_data_FGD1_type1_wome_fboff_minaclev0_100818/allTree.root
 fgd2=/data/t2k/coplowe/numuCC1P1PiOutput/march17/neutP6BWA/FGD2/nall_data_FGD2_type1_wome_fboff_minaclev0_090818/allTree.root
 
 jno=0
